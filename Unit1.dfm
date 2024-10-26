bbb8a2775844cf5eb4d2448d435c1b182085c6ba
Janilto Oliveira <55307672+jackanilto@users.noreply.github.com>
2024-10-26 13:56:35 -0300

    Initial commit

---
+++ Unit1.dfm
@@ -0,0 +1,63 @@
+object Form1: TForm1
+  Left = 0
+  Top = 0
+  Caption = 'Form1'
+  ClientHeight = 367
+  ClientWidth = 561
+  Color = clAppWorkSpace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -12
+  Font.Name = 'Segoe UI'
+  Font.Style = []
+  OnShow = FormShow
+  TextHeight = 15
+  object Panel1: TPanel
+    Left = 344
+    Top = 24
+    Width = 185
+    Height = 153
+    Caption = 'Panel1'
+    Color = clSilver
+    ParentBackground = False
+    TabOrder = 0
+  end
+  object Edit1: TEdit
+    Left = 16
+    Top = 64
+    Width = 169
+    Height = 23
+    BorderStyle = bsNone
+    Color = clSilver
+    TabOrder = 1
+    Text = 'Edit1'
+  end
+  object Button1: TButton
+    Left = 16
+    Top = 104
+    Width = 169
+    Height = 57
+    Caption = 'Button1'
+    TabOrder = 2
+  end
+  object ComboBox1: TComboBox
+    Left = 16
+    Top = 176
+    Width = 169
+    Height = 23
+    Color = clBtnFace
+    TabOrder = 3
+    Text = 'ComboBox1'
+  end
+  object GroupBox1: TGroupBox
+    Left = 344
+    Top = 240
+    Width = 185
+    Height = 105
+    Caption = 'GroupBox1'
+    Color = clSilver
+    ParentBackground = False
+    ParentColor = False
+    TabOrder = 4
+  end
+end
---
+++ Unit1.pas
@@ -0,0 +1,59 @@
+unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ExtCtrls,
+  Vcl.Imaging.jpeg, Vcl.Imaging.pngimage;
+
+type
+  TForm1 = class(TForm)
+    Panel1: TPanel;
+    Edit1: TEdit;
+    Button1: TButton;
+    ComboBox1: TComboBox;
+    GroupBox1: TGroupBox;
+    procedure FormShow(Sender: TObject);
+  private
+    { Private declarations }
+    procedure ArredondaCantos(Component: TWinControl; XRadius, YRadius: Integer); // Arredonda o componente especificado
+    procedure ConfigurarArredondamento; // Configura o arredondamento para cada componente
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+{ TForm1 }
+
+// Procedure para arredondar os cantos de um componente específico
+procedure TForm1.ArredondaCantos(Component: TWinControl; XRadius, YRadius: Integer);
+var
+  Rgn: HRGN;
+begin
+  Rgn := CreateRoundRectRgn(0, 0, Component.Width, Component.Height, XRadius, YRadius);
+  SetWindowRgn(Component.Handle, Rgn, True);
+end;
+
+// Procedure para configurar o arredondamento dos componentes
+procedure TForm1.ConfigurarArredondamento;
+begin
+  ArredondaCantos(Button1, 20, 20); // Arredondamento para Button1 com raio de 20x20
+  ArredondaCantos(Panel1, 20, 20);  // Arredondamento para Panel1 com raio de 20x20
+  ArredondaCantos(ComboBox1, 10, 10); // Exemplo de arredondamento para ComboBox1 com raio de 10x10 (personalizável)
+  ArredondaCantos(GroupBox1, 20, 20); // Exemplo de arredondamento para v com raio de 20x20 (personalizável)
+  ArredondaCantos(Edit1, 10, 10);     // Exemplo de arredondamento para Edit1 com raio de 10x10 (personalizável)
+end;
+
+procedure TForm1.FormShow(Sender: TObject);
+begin
+  ConfigurarArredondamento; // Chama a configuração de arredondamento ao iniciar o formulário
+end;
+
+end.
